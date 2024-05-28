8fb015a240be082967f216f8fd3a1c6905eb5761
myyrakle <sssang97@naver.com>
2024-05-29 00:17:22 +0900

    Create main.rs

---
+++ examples/operators/pod-watcher/src/main.rs
@@ -0,0 +1,31 @@
+use std::pin::pin;
+
+use futures::TryStreamExt;
+use k8s_openapi::api::core::v1::Pod;
+use kube::{
+    runtime::{
+        watcher::{watcher, Config},
+        WatchStreamExt,
+    },
+    Api, ResourceExt,
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = kube::Client::try_default().await?;
+
+    let pods: Api<Pod> = Api::default_namespaced(client);
+
+    let stream = watcher(pods, Config::default()).applied_objects();
+    let mut pinned_stream = pin!(stream);
+
+    while let Some(event) = pinned_stream.try_next().await? {
+        let pod_name = event.name_any();
+
+        let status = event.status.map(|e| e.phase).flatten();
+
+        println!("Pod: {} is in phase: {:?}", pod_name, status);
+    }
+
+    return Ok(());
+}
